a3576ee8ad7cf20e1e90752d4ef1fa6ce2db8d50
Michael Vaughn <mikish.vaughn@gmail.com>
2024-05-24 14:07:41 -0500

    Add more Rescources

--- APNaz/obj/Debug/net8.0/APNaz.AssemblyInfo.cs
+++ APNaz/obj/Debug/net8.0/APNaz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APNaz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+238c6e03bbacfeb5c822552056ededb28383cf2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f374a867f971ff47b8e0c148b0c2af50590b44e")]
 [assembly: System.Reflection.AssemblyProductAttribute("APNaz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APNaz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
